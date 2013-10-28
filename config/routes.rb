246608443e17988efa611d5a61945c2c05cc8b97
Hans <hanstrax@gmail.com>
2013-10-28 19:57:41 -0300

    add angular

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 AngularExample::Application.routes.draw do
   resources :users
-
+  root to: "static#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+
+names = ["Alfredo", "Bernardita", "Camilo", "Daniela", "Elena", "Francisco", "Gerardo", "Hodor", "Ignacio", "Jaimee", "Pedro", "Pablo", "Sebastián", "Franco", "Joaquín", "Gabriel", "Eduardo", "Camila"]
+last_names = ["Armas", "Benavente", "Contreras", "Caviedes", "Restaino", "Ariztía", "Vidal", "Ramirez", "Fernandes", "Lahm", "Lam"]
+
+puts "Start creating users"
+r = Random.new
+names.each do |name|
+	last_names.each do |last|
+		User.create({name: name, last_name: last, points: r.rand(100)})
+	end
+end
+puts "End creating users"
